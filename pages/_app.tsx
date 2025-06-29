c03ced53fcc600b3114744e7516590a7484a1f6a
KRHa <3dssmg2@gmail.com>
2025-06-29 22:16:51 +0900

    ✨ メタデータの設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Noto_Sans_JP, M_PLUS_Rounded_1c } from 'next/font/google';
+import Head from "next/head";
 
 const notoSansJP = Noto_Sans_JP({
   weight: ['300', '400', '500', '600', '700'],
@@ -17,6 +18,16 @@ const mPlusRounded = M_PLUS_Rounded_1c({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>くろはにほへと</title>
+        <meta name="description" content="ホームページ" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <style jsx global>{`
         .m-plus-rounded {
           font-family: ${mPlusRounded.style.fontFamily};
