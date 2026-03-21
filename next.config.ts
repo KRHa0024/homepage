9907541f6ff74e4b343b9276bfa31759c5ef7a28
KRHa <3dssmg2@gmail.com>
2026-03-21 19:00:04 +0900

    ✨ feat: 環境変数制限回避

--- next.config.ts
+++ next.config.ts
@@ -14,4 +14,18 @@ const nextConfig: NextConfig = {
   },
 };
 
+if (process.env.NEXT_PUBLIC_BLOB_BASE_URL) {
+  try {
+    const hostname = new URL(process.env.NEXT_PUBLIC_BLOB_BASE_URL).hostname;
+    nextConfig.images?.remotePatterns?.push({
+      protocol: 'https',
+      hostname: hostname,
+      port: '',
+      pathname: '/**',
+    });
+  } catch (e) {
+    console.error("Invalid NEXT_PUBLIC_BLOB_BASE_URL", e);
+  }
+}
+
 export default nextConfig;
